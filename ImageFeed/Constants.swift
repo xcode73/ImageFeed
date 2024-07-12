39842daeb8ce238d4b3a1501932c4d94e334ff11
Nikolai Eremenko <nikolai.dev@icloud.com>
2024-07-12 10:55:49 +0400

    docs: add comments

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 enum Constants {
+    // free access key
     static let accessKey = "9oTiliJ_ifDhqCbxo_ZPqy07nu8P8kF3dT-YKaBbkQ8"
+    // free secret key
     static let secretKey = "1bKp9BHyhOnu673zLIVt__fN1EskFvBAGnQn7vc92wA"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
